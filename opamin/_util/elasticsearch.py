ca48bbcb36b3176c2e14e212af21e69e89f3858c
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2020-04-18 16:28:56 +0200

    Refactor register_elasticsearch_connection (add typing).

--- opamin/_util/elasticsearch.py
+++ opamin/_util/elasticsearch.py
@@ -13,3 +13,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+from logging import Logger, getLogger
+from pathlib import Path
+from typing import Mapping, cast
+
+from elasticsearch_dsl import connections
+from typing_extensions import Final
+
+LOGGER: Final[Logger] = getLogger(__name__)
+
+
+def register_elasticsearch_connection(config: Mapping[str, object]) -> None:
+    c = cast(Mapping[str, object], config["elasticsearch-secrets"])  # Shortcut alias.
+    host = cast(str, c["host"])
+    port = cast(int, c["port"])
+    user = cast(str, c["user"])
+    password = cast(str, c["password"])
+    ca_crt_path = Path(cast(str, c["ca-crt-path"]))
+
+    if not ca_crt_path.exists():
+        raise FileNotFoundError(
+            f"CA-Certificate '{ca_crt_path}' could not be found "
+            "(configured in 'elasticsearch-secrets.ca-crt-path' of 'config.toml')."
+        )
+
+    LOGGER.debug("Registering Elasticsearch connection.")
+    connections.create_connection(
+        hosts=host,
+        port=port,
+        http_auth=(user, password),
+        # Use SSL
+        scheme="https",
+        use_ssl=True,
+        ssl_show_warn=True,
+        ssl_assert_hostname=host,
+        verify_certs=True,
+        ca_certs=str(ca_crt_path),
+        # Enable HTTP compression because we will probably insert a lot of large
+        # documents and the documentation says it will help:
+        # https://elasticsearch-py.readthedocs.io/en/master/#compression
+        http_compress=True,
+        # For development, so errors are seen comparatively fast.
+        max_retries=2,
+        timeout=3,
+    )
